af29f067dead51a8fec9f76189774504a8f51d54
Vanlalhriata <vanlalhriata@gmail.com>
2019-08-29 17:44:24 +0530

    Implement camera feed

---
+++ HlslCamera/HlslCamera/Cameramanager.cs
@@ -0,0 +1,58 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+
+namespace HlslCamera
+{
+    public class CameraManager
+    {
+        public event EventHandler OnNewFrame;
+
+        public Bitmap CurrentBitmap { get; private set; }
+
+        private VideoCaptureDevice videoSource;
+
+        public CameraManager()
+        {
+            Application.Current.Exit += application_Exit;
+        }
+
+        public void InitializeVideo()
+        {
+            var videoDevicesList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevicesList.Count < 1)
+            {
+                // TODO: Error
+            }
+            else
+            {
+                videoSource = new VideoCaptureDevice(videoDevicesList[videoDevicesList.Count - 1].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            CurrentBitmap = eventArgs.Frame;
+            OnNewFrame?.Invoke(this, null);
+        }
+
+        private void application_Exit(object sender, ExitEventArgs e)
+        {
+            videoSource?.Stop();
+
+            if (null != OnNewFrame)
+            {
+                var handlers = OnNewFrame.GetInvocationList();
+                foreach (EventHandler handler in handlers)
+                {
+                    OnNewFrame -= handler;
+                }
+            }
+        }
+    }
+}
--- HlslCamera/HlslCamera/MainWindow.xaml.cs
+++ HlslCamera/HlslCamera/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace HlslCamera
 {
@@ -7,11 +11,40 @@ namespace HlslCamera
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CameraManager cameraManager;
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = new MainViewModel();
+            this.Loaded += mainWindow_Loaded;
         }
+
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            cameraManager = new CameraManager();
+            cameraManager.OnNewFrame += cameraManager_OnNewFrame;
+            cameraManager.InitializeVideo();
+        }
+
+        private void cameraManager_OnNewFrame(object sender, System.EventArgs e)
+        {
+            try
+            {
+                BitmapSource bitmapSource;
+                using (var bitmap = (Bitmap)cameraManager.CurrentBitmap.Clone())
+                {
+                    bitmapSource = Utils.ToBitmapSource(bitmap);
+                }
+                bitmapSource.Freeze(); // avoid cross thread operations and prevents leaks
+                Application.Current.Dispatcher.BeginInvoke(new ThreadStart(delegate { image.Source = bitmapSource; }));
+            }
+            catch (Exception exception)
+            {
+                // TODO: Error
+            }
+        }
+
     }
 }
